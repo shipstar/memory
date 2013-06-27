ae8b5dd6a3eac313c428354c4a7185f98a6aefa2
Kyle Shipley <kyleashipley@gmail.com>
2013-06-27 15:34:12 -0400

    Print location of touches.

--- app/game_layer.rb
+++ app/game_layer.rb
@@ -3,6 +3,7 @@ class GameLayer < Joybox::Core::Layer
 
   def on_enter
     load_sprite_sheet
+    handle_touches
 
     @grid = Grid.new(sprite_batch: @sprite_batch)
   end
@@ -11,5 +12,12 @@ class GameLayer < Joybox::Core::Layer
     Joybox::Core::SpriteFrameCache.frames.add file_name: "sprites.plist"
     @sprite_batch = Joybox::Core::SpriteBatch.new file_name: "sprites.png"
     self << @sprite_batch
+  end
+
+  def handle_touches
+    on_touches_ended do |touches, event|
+      touch = touches.any_object
+      puts [touch.location.x, touch.location.y].inspect
+    end
   end
 end
