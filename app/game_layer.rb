e03933f35ee49eb4b16ee794dbfb0cbeab602a2e
Kyle Shipley <kyleashipley@gmail.com>
2013-06-27 12:03:17 -0400

    Draw sprites in bottom left and upper right corners to understand boundaries.

--- app/game_layer.rb
+++ app/game_layer.rb
@@ -7,5 +7,7 @@ class GameLayer < Joybox::Core::Layer
     self << @sprite_batch
 
     @sprite_batch << Sprite.new(frame_name: 'boy.png', position: [Screen.half_width, Screen.half_height])
+    @sprite_batch << Sprite.new(frame_name: 'girl_cat.png', position: [0, 0])
+    @sprite_batch << Sprite.new(frame_name: 'girl_cat.png', position: [Screen.width, Screen.height])
   end
 end
