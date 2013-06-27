253e4b91de2fde1cdeb7644c84407366e438b5d6
Kyle Shipley <kyleashipley@gmail.com>
2013-06-27 12:01:17 -0400

    Draw a sprite in the middle of the screen.

--- app/game_layer.rb
+++ app/game_layer.rb
@@ -2,6 +2,10 @@ class GameLayer < Joybox::Core::Layer
   scene
 
   def on_enter
-    puts 'hello'
+    Joybox::Core::SpriteFrameCache.frames.add file_name: "sprites.plist"
+    @sprite_batch = Joybox::Core::SpriteBatch.new file_name: "sprites.png"
+    self << @sprite_batch
+
+    @sprite_batch << Sprite.new(frame_name: 'boy.png', position: [Screen.half_width, Screen.half_height])
   end
 end
