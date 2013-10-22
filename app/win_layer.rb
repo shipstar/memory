7e21236075ccfe85925e357b892f2d2803dd3a0d
Kyle Shipley <kyleashipley@gmail.com>
2013-10-22 10:58:59 -0400

    Add sound effect on win.

--- app/win_layer.rb
+++ app/win_layer.rb
@@ -5,5 +5,9 @@ class WinLayer < Joybox::Core::Layer
     title = Label.new text: "You Win!", font_size: 48,
       position: [Screen.half_width, Screen.half_height + 50]
     self << title
+
+    audio_effect = AudioEffect.new
+    audio_effect[:win] = 'hooray.wav'
+    audio_effect.play :win
   end
 end
